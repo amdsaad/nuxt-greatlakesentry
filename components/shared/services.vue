6c7ea46336dd0890795e1879383eaadc589318cb
amdsaad <saadamd@gmail.com>
2021-06-04 21:44:44 +0400

    hompage services

---
+++ components/shared/services.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="containerMn">
+    <h1>Proudly Serving the Michigan, Ohio, Indiana and Canada Regions</h1>
+    <div class="serviceGRoup">
+      <div v-for="(item,indx) in servicesdata" :key="indx" class="serviceGRoupitem">
+        <div class="aserviceImg">
+          <img :src="item.image.url" alt="service img">
+          <a href="/products/stationary-docks">
+            <h2>{{ item.title }}</h2>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    servicesdata: {
+      type: Array,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="homepage">
     <section v-for="(section,indx) in homepage.section" :id="section.cssId" :key="indx">
       <slider v-if="section.__component == 'groups.slick-slide-group'" :sliderdata="section.Slick_slide_item" />
+      <services v-if="section.__component == 'groups.service-group'" :servicesdata="section.service_item" />
     </section>
   </div>
 </template>

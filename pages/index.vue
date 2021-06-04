ed6ee06f45735c9d9ec5c05ea1057dfe223384c9
amdsaad <saadamd@gmail.com>
2021-06-04 21:55:13 +0400

    homepage doucks

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <section v-for="(section,indx) in homepage.section" :id="section.cssId" :key="indx">
       <slider v-if="section.__component == 'groups.slick-slide-group'" :sliderdata="section.Slick_slide_item" />
       <services v-if="section.__component == 'groups.service-group'" :servicesdata="section.service_item" />
+      <doucks v-if="section.__component == 'groups.doucks-group'" :doucksdataleft="section.doucksleft" :doucksdataright="section.doucksright" />
     </section>
   </div>
 </template>

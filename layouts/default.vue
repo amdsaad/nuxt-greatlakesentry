5a421ce3a5b7c6ea2b995f90d56edf952a955c07
amdsaad <saadamd@gmail.com>
2021-06-06 12:34:49 +0400

    footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div>
     <navbar />
     <Nuxt />
+    <app-footer />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,49 @@
 <template>
   <div class="homepage">
-    <section v-for="(section,indx) in homepage.section" :id="section.cssId" :key="indx">
-      <slider v-if="section.Slick_slide_item.length " :sliderdata="section.Slick_slide_item" />
-      <services v-if="section.__component == 'groups.service-group'" :servicesdata="section" />
-      <doucks v-if="section.__component == 'groups.doucks-group'" :doucksdataleft="section.doucksleft" :doucksdataright="section.doucksright" />
-      <testimonial v-if="section.__component == 'groups.testimonial-group'" :testimonialdata="section" />
-      <reviewGroup v-if="section.__component == 'groups.review-group'" :reviewgroupdata="section" />
-      <reviewGrouptwo v-if="section.__component == 'groups.review-group-two'" :reviewgrouptwodata="section" />
-      <reviewGroupThree v-if="section.__component == 'groups.review-group-three'" :reviewgroupthreedata="section" />
+    <section
+      v-for="(section, indx) in homepage.section"
+      :id="section.cssId"
+      :key="indx"
+    >
+      <slider
+        v-if="section.Slick_slide_item"
+        :sliderdata="section.Slick_slide_item"
+      />
+      <services
+        v-if="section.__component == 'groups.service-group'"
+        :servicesdata="section"
+      />
+      <doucks
+        v-if="section.__component == 'groups.doucks-group'"
+        :doucksdataleft="section.doucksleft"
+        :doucksdataright="section.doucksright"
+      />
+      <testimonial
+        v-if="section.__component == 'groups.testimonial-group'"
+        :testimonialdata="section"
+      />
+      <reviewGroup
+        v-if="section.__component == 'groups.review-group'"
+        :reviewgroupdata="section"
+      />
+      <reviewGrouptwo
+        v-if="section.__component == 'groups.review-group-two'"
+        :reviewgrouptwodata="section"
+      />
+      <reviewGroupThree
+        v-if="section.__component == 'groups.review-group-three'"
+        :reviewgroupthreedata="section"
+      />
     </section>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $strapi }) {
+  async asyncData({ $strapi }) {
     const homepage = await $strapi.find('homepage')
-    console.log(homepage)
+    // console.log(homepage)
     return { homepage }
-  }
+  },
 }
 </script>
